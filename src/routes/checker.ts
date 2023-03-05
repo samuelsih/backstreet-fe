a69d499def76d0051158c3a65313faea615da200
samuelsih <samuel.205025@mhs.its.ac.id>
2023-03-05 22:48:18 +0700

    remove unused

--- src/routes/checker.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const indexClickedBtn = writable(0);
--- src/routes/claim/[slug]/+page.server.ts
+++ src/routes/claim/[slug]/+page.server.ts
@@ -1,17 +1,13 @@
-import { getLink } from '$lib/server';
+import { getDataBySlug } from '$lib/server';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params }) => {
-	const { code, message, response } = await getLink(params.slug);
+	const { code, message, response } = await getDataBySlug(params.slug);
 
 	if (code >= 400) {
 		if (code >= 500 && message.includes('ECONNREFUSED')) {
 			throw error(code, 'Unexpected Error');
-		}
-
-		if(!["LINK", "FILE"].includes(response.type)) {
-			throw error(400, "Bad Request");
 		}
 
 		throw error(code, message);
