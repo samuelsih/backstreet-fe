33dc143ed08aca762153c65de544567a5293d62f
samuelsih <samuel.205025@mhs.its.ac.id>
2023-04-06 05:34:06 +0700

    chore: remove <in progress>

--- src/routes/file/+page.svelte
+++ src/routes/file/+page.svelte
@@ -15,7 +15,7 @@
 <div class="mt-[30vh] sm:mx-auto sm:w-full sm:max-w-md">
 	<section class="text-center">
 		<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-			<span class="block xl:inline"> File (In Progress) </span>
+			<span class="block xl:inline"> File </span>
 		</h1>
 	</section>
 
